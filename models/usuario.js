bd42606d97facca7f88ef1cab3b4ad3eb8d182e4
felixtitonina <felixtitonina@outlook.com>
2021-03-18 01:50:25 -0300

    [+] model user

--- models/usuario.js
+++ models/usuario.js
@@ -4,6 +4,7 @@ const uniqueValidator = require('mongoose-unique-validator')
 const cryto = require("crypto")
 const jwt = require("jsonwebtoken")
 const secret = require("../config").secret
+const { token } = require("morgan")
 
 const errorCampo = "Campo não pode ficar vazio"
 const errorEmail = "Campo inválido"
@@ -41,3 +42,52 @@ const UsuarioSchema = new mongoose.Schema({
         dafault: {}
     }
 }, { timestamps: true })
+
+UsuarioSchema.plugin(uniqueValidator, { message: "Já esta sendo utilizado" })
+
+UsuarioSchema.methods.setSenha = function (password) {
+    this.salt = cryto.randomBytes(16).toString("hex")
+    this.hash = cryto.pbkdf2Sync(password, this.salt, 10000, 512, "sha512").toString("hex")
+}
+
+UsuarioSchema.methods.validarSenha = function (password) {
+    const hash = crypto.pbkdf2Sync(password, this.salt, 10000, 512, "sha512").toString("hex")
+    return hash === this.hash
+}
+
+UsuarioSchema.methods.gerarToken = function () {
+    const hoje = new Date()
+    const sxp = new Date(today)
+    exp.setDate(today.getDate() + 15)
+
+    return jwt.sign({
+        id: this._id,
+        email: this.email,
+        nome: this.nome,
+        exp: parseFloat(exp.getTime() / 1000, 10)
+    }, secret)
+}
+
+UsuarioSchema.methods.enviarAuthJSON = function () {
+    return {
+        nome: this.nome,
+        email: this.email,
+        loja: this.loja,
+        role: this.permissao,
+        token: this.gerarToken()
+    }
+}
+
+UsuarioSchema.methods.criarTokenRecuperacaoSenha = function () {
+    this.recovery = {}
+    this.recovery.token = crypto.randomBytes(16).toString("hex")
+    this.recovery.date = new Date(new Date().getTime() + 24 * 60 * 60 * 1000)
+    return this.recovery
+}
+
+UsuarioSchema.methods.finalizarTokenRecuperacaoSenha = function () {
+    this.recovery = { token: null, date: null }
+    return this.recovery
+}
+
+module.exports = mongoose.model("Usuario", UsuarioSchema)
