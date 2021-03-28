7e514785bdf14619cddc17ead065c6215d937bab
felixtitonina <felixtitonina@outlook.com>
2021-03-28 00:11:11 -0300

    [+] validate routes

--- routes/api/v1/usuarios.js
+++ routes/api/v1/usuarios.js
@@ -10,8 +10,8 @@ const usuarioController = new UsuarioController()
 
 
 router.post("/login", validate(UsuarioValidation.login, /**{keyByField: true}, {}*/ ), usuarioController.login)
-router.post("/registrar", usuarioController.store)
-router.put("/", auth.required, usuarioController.update)
+router.post("/registrar", validate(UsuarioValidation.store), usuarioController.store)
+router.put("/", auth.required, validate(UsuarioValidation.update), usuarioController.update)
 router.delete("/", auth.required, usuarioController.remove)
 
 router.get("/recuperar-senha", usuarioController.showRecovery)
@@ -20,6 +20,6 @@ router.get("/senha-recuperada", usuarioController.showCompleteRecovery)
 router.post("/senha-recuperada", usuarioController.completeRecovery)
 
 router.get("/", auth.required, usuarioController.index)
-router.get("/:id", auth.required, usuarioController.show)
+router.get("/:id", auth.required, validate(UsuarioValidation.show), usuarioController.show)
 
 module.exports = router
