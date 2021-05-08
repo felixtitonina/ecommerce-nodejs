d88c82d4838fd501c2f68d17491fd9b3b41d7ae2
Felix Tito <56932963+felixtitonina@users.noreply.github.com>
2021-05-07 21:29:30 -0300

    Update email.js

--- config/email.js
+++ config/email.js
@@ -3,7 +3,7 @@ module.exports = {
     host: "smtp.gmail.com",
     port: 465,
     auth: { 
-        user: "felixtitonina@gmail.com",
-        pass: "SU54506083p0"
+        user: "",
+        pass: ""
     }
 }
